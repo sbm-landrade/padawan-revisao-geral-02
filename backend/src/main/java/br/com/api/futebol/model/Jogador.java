530d408465aba3f54cb29be6b6871d0b07930888
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-08-02 14:03:50 -0300

    Criei conexão e modelo jogador

--- backend/src/main/java/br/com/api/futebol/model/Jogador.java
+++ backend/src/main/java/br/com/api/futebol/model/Jogador.java
@@ -2,4 +2,42 @@ package br.com.api.futebol.model;
 
 public class Jogador {
 
+	private Long id;
+	private String nome;
+	private String time;
+
+	// Construtores
+	public Jogador() {
+	}
+
+	public Jogador(String nome, String time) {
+		this.nome = nome;
+		this.time = time;
+	}
+
+	// Getters e Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
 }
